59445e687e6769e9573ac8ba33e402c7e26f6377
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2020-07-08 11:39:42 -0400

    finish mvp and some stretch

---
+++ github-card/src/component/SearchBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import axios from 'axios'
+import SearchResultCard from './SearchResult'
+
+ const Search = props =>{
+     
+         return(
+             <div>
+                 <form onSubmit={props.searchSubmit}>
+                     <input 
+                        type="text"
+                        name="userText"
+                        value={props.userText}
+                        onChange={props.handleChange}
+                     />
+                    <button> Search </button>
+                 </form>
+               
+             </div>
+         )
+     
+ }
+ export default Search;
